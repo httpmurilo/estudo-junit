dbf7b3ad8a057b739842a46548176869ae4959b8
httpmurilo <muriloeduardo1997@outlook.com>
2022-01-22 12:58:16 -0300

    first commit

---
+++ estudo/src/main/java/io/murilo/junit/App.java
@@ -0,0 +1,13 @@
+package io.murilo.junit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ estudo/src/main/java/io/murilo/junit/junit/Calculator.java
@@ -0,0 +1,7 @@
+package io.murilo.junit.junit;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
